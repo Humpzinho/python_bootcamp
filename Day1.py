8fa3d2647edf7502280a0b65e1d2b6cf4bab8370
justHump <srdrone@hotmail.com>
2021-06-24 08:10:56 -0300

    > space

--- Day1.py
+++ Day1.py
@@ -3,10 +3,10 @@
 
 print("\nSeja Bem-vindo(a) ao gerador de nomes de bandas!\n");
 
-cidade = input("Qual é o nome da cidade que você nasceu?\n\n>");
+cidade = input("Qual é o nome da cidade que você nasceu?\n\n> ");
 print("\nLegal, eu adoro " + cidade + "!\n");
 
-animal = input("E, qual é o nome do seu animal de estimação?\n\n>");
+animal = input("E, qual é o nome do seu animal de estimação?\n\n> ");
 print("\nQue fofo!\n");
 
 print("O nome da sua banda podia ser " + animal + " " + cidade);
@@ -17,12 +17,10 @@ print("O nome da sua banda podia ser " + animal + " " + cidade);
 
 #print("\nWelcome to the band name generator!\n");
 
-#cidade = input("What is the name of the city you were born in?\n\n>" );
+#cidade = input("What is the name of the city you were born in?\n\n> " );
 #print("\nCool, i love " + cidade + "!\n");
 
-#animal = input("And, what is your pet's name?\n\n>");
+#animal = input("And, what is your pet's name?\n\n> ");
 #print("\nOwn, So cute!\n");
 
 #print("Your band name could be " + animal + " " + cidade);
-
-#Test one
