f23b971467c24bd9666de4353e8e5a1f93dc8726
justHump <srdrone@hotmail.com>
2021-06-24 07:51:55 -0300

    Portuguese Day1

--- Day1.py
+++ Day1.py
@@ -1,27 +1,26 @@
 
 #In Portuguese
 
-#print("Seja Bem-vindo(a) ao gerador de nomes de bandas!\n");
+print("Seja Bem-vindo(a) ao gerador de nomes de bandas!\n");
 
-#cidade = input("Qual é o nome da cidade que você nasceu?\n");
-#print("\nLegal, eu adoro " + cidade + "!\n");
+cidade = input("Qual é o nome da cidade que você nasceu?\n");
+print("\nLegal, eu adoro " + cidade + "!\n");
 
-#animal = input("E, qual é o nome do seu animal de estimação?\n");
-#print("\nQue fofo!\n");
+animal = input("E, qual é o nome do seu animal de estimação?\n");
+print("\nQue fofo!\n");
 
-#print("O nome da sua banda podia ser " + animal + " " + cidade);
+print("O nome da sua banda podia ser " + animal + " " + cidade);
 
 ##########
 
 #In English
 
-print("Welcome to the band name generator!\n");
+#print("Welcome to the band name generator!\n");
 
-cidade = input("What is the name of the city you were born in?\n");
-print("\nCool, i love " + cidade + "!\n");
+#cidade = input("What is the name of the city you were born in?\n");
+#print("\nCool, i love " + cidade + "!\n");
 
-animal = input("And, what is your pet's name?\n");
-print("\nOwn, So cute!\n");
+#animal = input("And, what is your pet's name?\n");
+#print("\nOwn, So cute!\n");
 
-print("Your band name could be " + animal + " " + cidade);
-
+#print("Your band name could be " + animal + " " + cidade);
